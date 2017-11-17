9119426d8cdb36e110b64c40d173be1eca3e1837
calebjay <rogerjscaleb@gmail.com>
2017-11-17 10:51:20 -0800

    vue cli

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,23 @@
 <template>
-<div class="message">{{ msg }}</div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
 
 <script>
 export default {
-    data () {
-        return {
-            msg: 'Hello from vue-loader!'
-        }
-    }
+  name: 'app'
 }
 </script>
 
 <style>
-    .message {
-        color: blue;
-    }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
