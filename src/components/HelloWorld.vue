36271dbfef72c084b2b17fa103ac5fd948db8c6b
calebjay <rogersjcaleb@gmail.com>
2017-12-01 12:22:46 -0800

    The math checks out

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,11 +20,15 @@
         >
       </input-with-descriptor>
     </div>
+    Working {{hours_week}} hours a week, or {{hours_day}} hours a day for {{days_in_year}} days of the year, 
+    it will take you approximately {{years_until_complete}} years, or {{worked_days_needed}} worked days to achieve 20000 hours. 
+    In {{weeks_until_complete}} weeks, you will have achieved your goal. That's {{days_until_complete}} days away.
   </div>
 </template>
 
 <script>
 import InputWithDescriptor from '@/components/InputWithDescriptor.vue'
+const HOURS_CONST = 10000;
 
 export default {
   name: 'HelloWorld',
@@ -34,9 +38,39 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      hours_day: '',
-      days_week: '',
+      hours_day: 0,
+      days_week: 0,
     }
+  },
+  computed: {
+    hours: function(){
+      return Number(this.hours_day);
+    },
+    days: function(){
+      return Number(this.days_week);
+    },
+    hours_week: function(){
+      return this.hours * this.days;
+    },
+    days_in_year: function(){
+      // return 365 - ((7 * 52) - (days * 52));
+      return this.days * 52;
+    },
+    hours_worked_per_year: function(){
+      return hours_worked = this.hours * this.days_in_year; 
+    },
+    worked_days_needed: function(){
+      return (HOURS_CONST / this.hours);
+    },
+    days_until_complete: function(){
+      return this.weeks_until_complete * 7;
+    },
+    weeks_until_complete: function(){
+      return this.worked_days_needed / this.days;
+    },
+    years_until_complete: function(){
+      return this.days_until_complete / 365;
+    },
   }
 }
 </script>
